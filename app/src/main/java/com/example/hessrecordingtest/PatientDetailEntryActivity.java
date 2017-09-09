d3e07d26a547fa72d009f8e272ae6a89abf35201
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-09-09 20:03:02 +0530

    Initial commit :coffee:

---
+++ app/src/main/java/com/example/hessrecordingtest/PatientDetailEntryActivity.java
@@ -0,0 +1,15 @@
+package com.example.hessrecordingtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientDetailEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_detail_entry);
+    }
+
+    
+}
